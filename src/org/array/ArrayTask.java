bd1322d14f70d3c2bb55d2f6518b1813060984e5
DivyaBharathigit <baskaran.divya29@gmail.com>
2024-02-22 15:50:47 +0530

    Done

---
+++ src/org/array/ArrayTask.java
@@ -0,0 +1,58 @@
+package org.array;
+
+public class ArrayTask {
+
+	public static void main(String[] args) {
+		
+		String t2="Welcome To Java ";
+		int vowels=0, consonants=0, numbers=0, specialchar=0;
+ for(int i=0;i<t2.length();i++) {
+	 char ch=t2.charAt(i);
+	 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+		 vowels++;
+	 }
+	 else {
+		 consonants++;
+	 }
+	 System.out.println("vowels count:" +vowels);
+	 System.out.println("consonants count:" +consonants);
+ }
+		
+	}	
+}	
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
